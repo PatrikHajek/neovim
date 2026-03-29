eb72bf9ceee1b4f78cc77fd56d3d6bcb71eb6d75
PatrikHajek <hajan21@seznam.cz>
2026-03-29 14:17:23 +0200

    feat: add `g` flag to current buffer replace keymap

--- lua/custom/config.lua
+++ lua/custom/config.lua
@@ -247,8 +247,8 @@ vim.keymap.set('n', '<leader><CR>', function()
 end, { desc = 'Search line under the cursor ignoring indentation' })
 
 vim.keymap.set('n', '/', '/\\v', { desc = 'Enable very magic for searching', noremap = true })
-vim.keymap.set('n', '<leader>br', ':%s//', { desc = '[B]uffer [R]eplace' })
-vim.keymap.set('v', '<leader>br', ':s//', { desc = '[B]uffer [R]eplace in selected lines' })
+vim.keymap.set('n', '<leader>br', ':%s///g<Left><Left>', { desc = '[B]uffer [R]eplace' })
+vim.keymap.set('v', '<leader>br', ':s///g<Left><Left>', { desc = '[B]uffer [R]eplace in selected lines' })
 
 -- keep cursor in the middle when searching
 vim.keymap.set('n', 'n', 'nzzzv', { silent = true })
