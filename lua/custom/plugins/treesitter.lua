9d3fa20de436e98f03f85889d2098453277f9795
PatrikHajek <hajan21@seznam.cz>
2026-04-04 16:24:59 +0200

    refactor: move nvim-autopairs to treesitter.lua

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -35,4 +35,18 @@ return {
   },
 
   'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
 }
