32fef85871523ad4d94fea14ee63ea5154162c42
PatrikHajek <hajan21@seznam.cz>
2026-04-04 14:52:16 +0200

    refactor: move nvim-surround into coding.lua

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -460,6 +460,12 @@ return {
   },
 
   {
+    'kylechui/nvim-surround',
+    version = '^4.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+  },
+
+  {
     'chrisgrieser/nvim-spider',
     lazy = true,
     keys = {
