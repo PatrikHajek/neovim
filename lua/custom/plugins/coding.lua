1b023d76557d8167d6845660409d83f5ebd55225
PatrikHajek <hajan21@seznam.cz>
2026-04-06 17:06:24 +0200

    refactor: add dependency for easier maintenance

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -352,6 +352,7 @@ return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     dependencies = {
+      'kiyoon/repeatable-move.nvim',
       'nvim-treesitter/nvim-treesitter',
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
