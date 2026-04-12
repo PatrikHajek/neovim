2a80ee73464f56754278ea84cad4f518854d10f4
PatrikHajek <hajan21@seznam.cz>
2026-04-12 17:42:13 +0200

    feat: map `.axaml` to HTML filetype

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -1,5 +1,13 @@
 -- For C# LSP.
 
+vim.filetype.add {
+  extension = {
+    -- For Avalonia development.
+    -- XML doesn't have textobjects, so using HTML instead.
+    axaml = 'html',
+  },
+}
+
 return {
   'seblyng/roslyn.nvim',
   ---@module 'roslyn.config'
