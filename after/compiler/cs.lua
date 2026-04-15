f92ed5f68967a715326304cfcdcf917238e91d57
PatrikHajek <hajan21@seznam.cz>
2026-04-15 21:10:16 +0200

    feat: don't capture duplicate messages from compiler output

--- after/compiler/cs.lua
+++ after/compiler/cs.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_buf_set_var(0, 'current_compiler', 'cs')
 
-vim.opt_local.makeprg = 'dotnet build'
+--@ Using `--no-restore` to avoid repeated messages.
+vim.opt_local.makeprg = 'dotnet build --no-restore'
 
 vim.opt_local.errorformat = {
   [[%f(%l\,%c):\ %trror\ %.%\\+:\ %m]],
